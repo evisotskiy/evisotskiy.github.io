89bb4d74cbdb507be7cd039a97874c8889163b47
Evgene <evisotskiy@zaraffasoft.com>
2018-12-08 20:02:55 +0200

    Storybook refactoring

---
+++ rentivo-search-widget/static/iframe.bd943832660a7a7dece2.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{451:function(module,exports,__webpack_require__){__webpack_require__(194),__webpack_require__(452),module.exports=__webpack_require__(453)},453:function(module,exports){}},[[451,2,4]]]);
+//# sourceMappingURL=iframe.bd943832660a7a7dece2.bundle.js.map
